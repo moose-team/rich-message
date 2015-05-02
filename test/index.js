1fdaba770bdbe8cab17cdb995a211189f251b808
ekmartin <mail@ekmartin.no>
2015-05-02 11:12:09 +0200

    Fix link regex (#8) and add test

--- test/index.js
+++ test/index.js
@@ -1,1 +1,19 @@
+var test = require('tape')
+var richMessage = require('../')
 
+test('link replacement', function (t) {
+  var message = {
+    text: '#cats #cats #cats not#cat',
+    username: 'cat',
+    timestamp: Date.now()
+  }
+
+  var output = richMessage(message, 'other_cat')
+  var expected = '<div><p>' +
+    '<a href="#cats">#cats</a> <a href="#cats">#cats</a> ' +
+    '<a href="#cats">#cats</a> not#cat' +
+    '</p><p></p></div>'
+
+  t.equal(output.html, expected)
+  t.end()
+})
