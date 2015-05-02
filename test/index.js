0e4a7d9b4d561e7c5ed87e4995b3feecf56dc2b8
ekmartin <mail@ekmartin.no>
2015-05-02 11:19:44 +0200

    Add avatar tests and replace tap-spec with tap-dot

--- test/index.js
+++ test/index.js
@@ -17,3 +17,27 @@ test('link replacement', function (t) {
   t.equal(output.html, expected)
   t.end()
 })
+
+test('github avatars', function (t) {
+  var message = {
+    text: 'i like cats',
+    username: 'cat',
+    timestamp: Date.now()
+  }
+
+  var output = richMessage(message, 'other_cat')
+  t.equal(output.avatar, 'https://github.com/cat.png')
+  t.end()
+})
+
+test('anon avatars', function (t) {
+  var message = {
+    text: 'i like cats',
+    username: 'Anonymous cat',
+    timestamp: Date.now()
+  }
+
+  var output = richMessage(message, 'other_cat')
+  t.equal(output.avatar, 'static/cat.png')
+  t.end()
+})
